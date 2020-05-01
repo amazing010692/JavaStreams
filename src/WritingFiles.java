b55e5efee4fb9fa74afc0f1ace7782af6725b06c
Janielle Gregorio <hello_janielle1992@yahoo.com.ph>
2020-05-02 00:43:54 +0800

    Writing in TXT and CSV Files

---
+++ src/WritingFiles.java
@@ -0,0 +1,43 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WritingFiles {
+
+	public static void main(String[] args) throws IOException {
+		
+		//Stream Connectivity
+		File f = new File("C:\\Users\\Janielle Gregorio\\Documents\\Java_Tests\\JavaStreams\\src\\MyTextFile.csv");
+		FileWriter fw = new FileWriter(f, true);
+		BufferedWriter writer = new BufferedWriter(fw);
+		
+		for(int i = 0; i < 4; i++) {
+			
+			for(int j = 0; j < 3; j++) {
+				
+				int num = (int)(Math.random() * 100);
+				writer.write(num + ",");
+				
+			}
+			writer.newLine();
+		}
+		
+		//Writing Inside the File
+		/*writer.newLine();
+		writer.write("First Line");
+		writer.newLine();
+		writer.write("Philippines");
+		writer.newLine();
+		writer.write("Way2Automation");
+		writer.newLine();
+		writer.write("Janielle");*/
+		
+		//Closing Stream
+		writer.close();
+		
+		System.out.println("File Created !!!");
+
+	}
+
+}
